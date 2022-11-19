b006e79faded597e0b4c2096f8e242397fa6f740
Melvin-King <79367726+Melvin-King@users.noreply.github.com>
2022-11-19 20:12:29 +0800

    Update DBController.java

--- src/main/java/comp4342/grp15/gem/DAO/DBController.java
+++ src/main/java/comp4342/grp15/gem/DAO/DBController.java
@@ -6,7 +6,17 @@ import comp4342.grp15.gem.Model.UploadMeta;
 import java.util.ArrayList;
 
 public class DBController {
-
+    static OracleConnection conn;
+    static FileOutputStream stream;
+    
+    static {
+        try {
+            stream = new FileOutputStream("SQL_File.txt");
+            stream.write(("Record All SQL statements at " + new Date() + "\n").getBytes());
+        } catch (Exception ignored) {
+        }
+    }
+    
     // get the latest post items, refer to 文档 从服务端获取动态的Json
     public ArrayList<ClientPostMeta> getTrends(){
         return null;
